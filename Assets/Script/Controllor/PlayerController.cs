849b661142428367dee910c5a7b798ece6e7d1dd
18023217296 <841223400@qq.com>
2023-10-22 20:56:27 +0800

    add animator

--- Assets/Script/Controllor/PlayerController.cs
+++ Assets/Script/Controllor/PlayerController.cs
@@ -5,11 +5,12 @@ using UnityEngine.AI;
 public class PlayerController : MonoBehaviour
 {
     private NavMeshAgent agent;
+    private Animator animator;
     // Start is called before the first frame update
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
-
+        animator = GetComponent<Animator>(); 
         // you can't put it here
         // MouseManager.Instance.MouseEventClick += OnMouseClick;
     }
@@ -22,7 +23,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        animator.SetFloat("Speed", agent.velocity.sqrMagnitude);
     }
 
     public void OnMouseClick(Vector3 target)
