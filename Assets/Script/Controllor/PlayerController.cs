1a75f3301a16b6320868a0870b4f82b6e3152ffb
18023217296 <841223400@qq.com>
2023-11-02 20:31:13 +0800

    update

--- Assets/Script/Controllor/PlayerController.cs
+++ Assets/Script/Controllor/PlayerController.cs
@@ -10,6 +10,8 @@ public class PlayerController : MonoBehaviour
 
 
     private GameObject attackTarget;
+
+    // cd is defined other way 
     private float lastAttackTime;
     // Start is called before the first frame update
     void Awake()
@@ -32,12 +34,43 @@ public class PlayerController : MonoBehaviour
         animator.SetFloat("Speed", agent.velocity.sqrMagnitude);
     }
 
+    private void FixedUpdate()
+    {
+        lastAttackTime -= Time.fixedDeltaTime;
+    }
+
+    // move to destinatiion
     public void OnMouseClickGround(Vector3 target)
     {
         agent.destination = target;
     }
+
+    // attack enemy after we click it
     private void OnMouseClickEnemy(GameObject obj)
     {
-        throw new NotImplementedException();
+        if (obj != null)
+        {
+            attackTarget = obj;
+        }
+    }
+
+    IEnumerator MoveToAttackTarget()
+    {
+        agent.isStopped = false;
+        transform.LookAt(attackTarget.transform);
+
+        // move until close enough, attack distance hard coded to 1, and will get from weapon later.
+        while (Vector3.Distance(transform.position, attackTarget.transform.position) > 1)
+        {
+            agent.destination = attackTarget.transform.position;
+            yield return null;
+        }
+        // move 
+        agent.isStopped = true;
+        //CD is over
+        if (lastAttackTime < 0)
+        {
+
+        }
     }
 }
